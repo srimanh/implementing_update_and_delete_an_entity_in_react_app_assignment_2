c7b9bfef9d6327d9cd4efcba58a188d9d5f33928
RefinerAI <refinerai@example.com>
2025-03-20 14:13:53 +0530

    test 5

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import ItemList from "./components/ItemList";
 
 const API_URI = `http://${import.meta.env.VITE_API_URI}/doors`;
 
-
+// console.log("print")
 function App() {
   const [items, setItems] = useState([]);
 
